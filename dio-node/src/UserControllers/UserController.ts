f3ad7a79d0ca4274632fce6cd5747aff223519ee
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-17 18:57:18 -0300

    altera o fluxo da aplicação, adicionando validação para parâmetros em branco e mudando a criação do usuário para arquivo de service responsável 'services/UserService.ts'

--- dio-node/src/UserControllers/UserController.ts
+++ dio-node/src/UserControllers/UserController.ts
@@ -1,17 +1,23 @@
   import { Request, Response } from 'express'
-
-  const db = [
-    {
-      name: 'test',
-      email: 'test@example.com'
-    }
-  ]
+  import { UserService } from '../services/UserService'
   
+  const userService = new UserService;
+
   export class UserController{
     createUser = (req: Request, res: Response) => {
       const user = req.body;
-      db.push(user)
-      console.log(db)
+
+      if(!user.name) {
+        return res.status(400).json({message: 'Bad Request: Nome inválido.'})
+      }
+
+      userService.createUser(user.name, user.email)
       return res.status(201).json({message: 'User created'});
+    }
+
+    getAllUsers = (req: Request, res: Response) => {
+
+      const users = userService.getAllUsers()
+      return res.status(200).json(users)
     }
   }
