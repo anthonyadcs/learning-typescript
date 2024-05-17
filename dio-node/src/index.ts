f22124b6d864b9e750a3aed424f10855bac5b0ff
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-17 18:06:18 -0300

    adiciona métodos get e post para criação de usuário mediante uso da classe 'UserController'

--- dio-node/src/index.ts
+++ dio-node/src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello World Updated')
+import { UserController } from "./UserControllers/UserController";
+import express, {Request, Response} from 'express';
+
+
+const db = [
+  {
+    name: 'test',
+    email: 'test@example.com'
+  }
+]
+
+const userController = new UserController()
+
+
+const server = express();
+
+server.use(express.json())
+
+server.get('/', (req: Request, res: Response) => {
+  return res.status(200).json({message: 'DioBank API'});
+})
+
+server.post('/user', userController.createUser)
+
+server.listen(5000, () => {
+  console.log('Server on')
+});
