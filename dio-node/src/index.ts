b66ffcc712789d35a9e28dcd2ed07fb5b3cd216a
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-22 22:15:01 -0300

    adiciona o manipulador de rotas 'Router' do Express e o adiciona no 'index.ts' da aplicação

--- dio-node/src/index.ts
+++ dio-node/src/index.ts
@@ -1,26 +1,19 @@
+import { router } from "./services/routes";
 import { UserController } from "./UserControllers/UserController";
 import express, {Request, Response} from 'express';
 
+const server = express();
+server.use(express.json())
+server.use(router)
 
-const db = [
-  {
-    name: 'test',
-    email: 'test@example.com'
-  }
-]
 
 const userController = new UserController()
-
-
-const server = express();
-
-server.use(express.json())
 
 server.get('/', (req: Request, res: Response) => {
   return res.status(200).json({message: 'DioBank API'});
 })
 
-server.post('/user', userController.createUser)
+
 
 server.listen(5000, () => {
   console.log('Server on')
---
+++ dio-node/src/services/routes.ts
@@ -0,0 +1,9 @@
+import { UserController } from "./../UserControllers/UserController";
+import {Router} from 'express';
+
+export const router = Router()
+const userController = new UserController()
+
+router.get('/user', userController.getAllUsers)
+router.post('/user', userController.createUser)
+
