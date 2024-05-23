2d8b08970db58dbc5e7f52e0646cfdaedf4d4f52
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-22 22:11:41 -0300

    adiciona o construtor na classe para criação do userService para reutilização do mesmo de forma mais semântica e altera funcionamento dos serviços com a utilização do this

--- dio-node/src/UserControllers/UserController.ts
+++ dio-node/src/UserControllers/UserController.ts
@@ -1,9 +1,13 @@
   import { Request, Response } from 'express'
   import { UserService } from '../services/UserService'
-  
-  const userService = new UserService;
 
   export class UserController{
+    userService: UserService;
+    
+    constructor(userService = new UserService()){
+      this.userService = userService;
+    }
+
     createUser = (req: Request, res: Response) => {
       const user = req.body;
 
@@ -11,13 +15,12 @@
         return res.status(400).json({message: 'Bad Request: Login inválido.'})
       }
 
-      userService.createUser(user.name, user.email)
-      return res.status(201).json({message: 'User created'});
+      this.userService.createUser(user.name, user.email)
+      return res.status(201).json({message: 'Usuário criado'});
     }
 
     getAllUsers = (req: Request, res: Response) => {
-
-      const users = userService.getAllUsers()
+      const users = this.userService.getAllUsers()
       return res.status(200).json(users)
     }
   }
