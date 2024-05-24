0c2e9dbd0103f159348444ae2ecf4196c9916124
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-23 22:44:51 -0300

    adiciona testes em casos de possível erro na função 'createUser', da função 'getAllUsers' e da função 'deleteUser'

--- dio-node/src/UserControllers/UserController.spec.ts
+++ dio-node/src/UserControllers/UserController.spec.ts
@@ -6,23 +6,64 @@ import {Request} from 'express'
 
 describe('UserController', () => {
     const mockUserService: Partial<UserService> = {
-      createUser: jest.fn()
+      createUser: jest.fn(),
+      getAllUsers: jest.fn(),
+      deleteUser: jest.fn(),
     }
 
     const userController = new UserController(mockUserService as UserService)
-
-    it('deve adicionar um novo usuário', () => {
-      const mockRequest = {
-        body: {
-          name: 'insert by test',
-          email: 'insertbytest@example.com'
-        }
-      } as Request
-
-      const mockResponse = makeMockResponse()
-      userController.createUser(mockRequest, mockResponse)
-
-      expect(mockResponse.state.status).toBe(201)
-      expect(mockResponse.state.json).toEqual({message: 'Usuário criado'})
+  
+    describe('createUser', () => {
+      it('deve adicionar um novo usuário', () => {
+        const mockRequest = {
+          body: {
+            name: 'insert by test',
+            email: 'insertbytest@example.com'
+          }
+        } as Request
+  
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+  
+        expect(mockResponse.state.status).toBe(201)
+        expect(mockResponse.state.json).toEqual({message: 'user created'})
+      })
+  
+      it('Deve retornar uma mensagem de erro se nome ou email não forem informados', () => {
+        const mockRequest = {
+            body: {
+                name: 'aaaa',
+                email: ''
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'bad request! name or email not filled in'})
+      })
     })
+
+  describe('getAllUsers', () => {
+      it('deve chamar a função getAllUsers', () => {
+          const mockRequest = {body: {} } as Request
+          const mockResponse = makeMockResponse()
+          userController.getAllUsers(mockRequest, mockResponse)
+          expect(mockResponse.state.status).toBe(200)
+      })
+  })
+
+  describe('deleteUser', () => {
+      it('deve excluir o usuário informado', () => {
+          const mockRequest = {body: {
+              name: "req test",
+              email: "reqtest@example.com",
+          }} as Request;
+
+          const mockResponse = makeMockResponse();
+
+          userController.deleteUser(mockRequest, mockResponse)
+          expect(mockResponse.state.status).toBe(202)
+          expect(mockResponse.state.json).toMatchObject({message: 'user deleted'})
+      })
+  })
 })
