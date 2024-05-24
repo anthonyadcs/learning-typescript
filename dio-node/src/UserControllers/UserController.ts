54793c319c184aa207b5e9d2c994a6b99b967108
anthonyadcs <anthonydacsilva10@gmail.com>
2024-05-23 22:42:22 -0300

    adiciona função que solicita aos serviços do usuário a função que deleta o usuário informado e em resultado positivo, retorna o status 202 e a mensagem de usuário deletado

--- dio-node/src/UserControllers/UserController.ts
+++ dio-node/src/UserControllers/UserController.ts
@@ -12,15 +12,21 @@
       const user = req.body;
 
       if(!user.name || !user.email) {
-        return res.status(400).json({message: 'Bad Request: Login inválido.'})
+        return res.status(400).json({message: 'bad request! name or email not filled in'})
       }
 
       this.userService.createUser(user.name, user.email)
-      return res.status(201).json({message: 'Usuário criado'});
+      return res.status(201).json({message: 'user created'});
     }
 
     getAllUsers = (req: Request, res: Response) => {
       const users = this.userService.getAllUsers()
       return res.status(200).json(users)
+    }
+
+    deleteUser = (req: Request, res: Response) => {
+      const user = req.body
+      this.userService.deleteUser(user.name, user.email)
+      return(res.status(202).json({ message: 'user deleted'}))
     }
   }
